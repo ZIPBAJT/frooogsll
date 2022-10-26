058c44d3196ee51a4f5580e99683998110c89b48
Miroslav <zibajzip@gmail.com>
2022-10-26 20:06:50 +0300

    11

--- components/Nav.js
+++ components/Nav.js
@@ -1,11 +1,11 @@
 import { Button,} from 'antd';
 import styles from '../styles/Nav.module.css'
 export function Nav(){
-    return (<>
+    return (<div className={styles.dive}>
     <Button className={styles.butt} href="/scetchess" >WWW</Button> 
     <Button className={styles.butt} href="/ikone">ikone,first</Button>
     <Button className={styles.butt} href="/telegun">telegramm</Button>
     <Button className={styles.butt} href="/buttondownload">button dowenload</Button>
-    </>
+    </div>
     )
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import '../styles/globals.css'
 import 'antd/dist/antd.css'
 import Head from 'next/head'
-import { Nav } from '../components/nav'
+import { Nav } from '../components/Nav'
 function MyApp({ Component, pageProps }) {
   return (
   <>
