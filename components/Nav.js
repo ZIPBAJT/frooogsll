bdfb0afb8354eb5b5b10e34db59f76e19535d3af
Miroslav <zibajzip@gmail.com>
2022-10-26 19:24:48 +0300

    w345r5t

--- components/Nav.js
+++ components/Nav.js
@@ -1,7 +1,11 @@
 import { Button,} from 'antd';
+import styles from '../styles/Nav.module.css'
 export function Nav(){
     return (<>
-    <Button href="/scetchess">WWW</Button> 
+    <Button className={styles.butt} href="/scetchess" >WWW</Button> 
+    <Button className={styles.butt} href="/ikone">ikone,first</Button>
+    <Button className={styles.butt} href="/telegun">telegramm</Button>
+    <Button className={styles.butt} href="/buttondownload">button dowenload</Button>
     </>
     )
 }
--- pages/scetchess.js
+++ pages/scetchess.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/scetchess.module.css'
+import styles from '../styles/scetchess.module.css'  // СТИЛИ
 
 
 export default function Home() {
@@ -11,7 +11,10 @@ export default function Home() {
     <>
       <div style={{margin: "50px"}}>
         <a href="https://editor.p5js.org/rembbole/sketches/uaCTwXWvZ">
-          <img className={styles.img} src="/a312b678P5jS.png" width={200} />
+          <img 
+            className={styles.img} // СТИЛИ
+             src="/a312b678P5jS.png" width={200} 
+             />
         </a>
       </div>
     </>
