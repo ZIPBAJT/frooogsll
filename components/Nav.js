3a223c49a83fc489b70e9c6f8ddd0cc61446d109
Miroslav <zibajzip@gmail.com>
2022-10-27 16:48:13 +0300

    COMMITT

--- components/Nav.js
+++ components/Nav.js
@@ -2,10 +2,12 @@ import { Button,} from 'antd';
 import styles from '../styles/Nav.module.css'
 export function Nav(){
     return (<div className={styles.dive}>
+        <div className={styls.sok}>
     <Button className={styles.butt} href="/scetchess" >WWW</Button> 
     <Button className={styles.butt} href="/ikone">ikone,first</Button>
     <Button className={styles.butt} href="/telegun">telegramm</Button>
     <Button className={styles.butt} href="/buttondownload">button dowenload</Button>
-    </div>
+ </div>
+ </div>
     )
 }
