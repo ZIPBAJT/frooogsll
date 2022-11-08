3408c2003bef9047c9c943250d2f5c075bd5b333
Miroslav <zibajzip@gmail.com>
2022-11-08 15:19:32 +0200

    COLLLORSS

--- components/Nav.js
+++ components/Nav.js
@@ -1,13 +1,34 @@
-import { Button,} from 'antd';
-import styles from '../styles/Nav.module.css'
-export function Nav(){
-    return (<div className={styles.dive}>
-        <div className={styls.sok}>
-    <Button className={styles.butt} href="/scetchess" >WWW</Button> 
-    <Button className={styles.butt} href="/ikone">ikone,first</Button>
-    <Button className={styles.butt} href="/telegun">telegramm</Button>
-    <Button className={styles.butt} href="/buttondownload">button dowenload</Button>
- </div>
- </div>
-    )
+import { Button } from "antd";
+import styles from "../styles/Nav.module.css";
+export function Nav() {
+  return (
+    <div className={styles.dive}>
+      <div className={styles.sok}>
+        <button
+          className={styles.butt}
+          onClick={() => (window.location.href = "/scetchess")}
+        >
+          scetches_of_p5
+        </button>
+        <button
+          className={styles.butt}
+          onClick={() => (window.location.href = "/ikone")}
+        >
+          ikone
+        </button>{" "}
+        <button
+          className={styles.butt}
+          onClick={() => (window.location.href = "/telegun")}
+        >
+          telegramm
+        </button>{" "}
+        <button
+          className={styles.butt}
+          onClick={() => (window.location.href = "/buttondownload")}
+        >
+          buttons_download
+        </button>
+      </div>
+    </div>
+  );
 }
